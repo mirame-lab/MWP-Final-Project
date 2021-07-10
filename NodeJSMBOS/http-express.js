960906cf375323f08b3cd707b6b544e42bdb0921
nurirdinaaa <irdinarifdi@gmail.com>
2021-07-10 12:21:42 +0800

    delete orderlist

--- NodeJSMBOS/http-express.js
+++ NodeJSMBOS/http-express.js
@@ -9,7 +9,7 @@ app.use(express.static('public')); // directory publish static files
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     //res.send('Hello! welcome to http-express.js Web Service');
     return res.redirect('/menus.html');
 });
@@ -18,7 +18,7 @@ app.get('/', function (req, res) {
 var db = config.dbfs;
 
 // GET method - query string
-app.get('/plus2num/', function (req, res) {
+app.get('/plus2num/', function(req, res) {
     var num1 = req.query.num1;
     var num2 = req.query.num2;
 
@@ -30,7 +30,7 @@ app.get('/plus2num/', function (req, res) {
 });
 
 // POST method - JSON data
-app.post('/plus2num/', function (req, res) {
+app.post('/plus2num/', function(req, res) {
     console.log(req.body);
     var num1 = req.body.num1;
     var num2 = req.body.num2;
@@ -43,7 +43,7 @@ app.post('/plus2num/', function (req, res) {
 });
 
 // GET method - REST
-app.get('/plus2num/:num1/:num2', function (req, res) {
+app.get('/plus2num/:num1/:num2', function(req, res) {
     var num1 = req.params.num1;
     var num2 = req.params.num2;
 
@@ -56,7 +56,7 @@ app.get('/plus2num/:num1/:num2', function (req, res) {
 
 
 // POST method - REST
-app.post('/plus2num/:num1/:num2', function (req, res) {
+app.post('/plus2num/:num1/:num2', function(req, res) {
     var num1 = req.params.num1;
     var num2 = req.params.num2;
 
@@ -69,7 +69,7 @@ app.post('/plus2num/:num1/:num2', function (req, res) {
 
 
 // Test Firebase query
-app.get('/courses', function (req, res) {
+app.get('/courses', function(req, res) {
     db.collection('courses').get().then((docs) => {
         var courses = [];
 
@@ -88,14 +88,14 @@ app.get('/courses', function (req, res) {
 ///////////////////////////////////////////////////////////////////////////////
 
 // Paths for menus ////////////////////////////////////////
-app.get('/mbos/menus', function (req, res) {
+app.get('/mbos/menus', function(req, res) {
     console.log(`/mbos/menus`);
     res.setHeader('Content-type', 'text/plain');
     res.setHeader('Access-Control-Allow-Origin', '*');
     return res.redirect('/mbos/menus/id/asc');
 });
 
-app.get('/mbos/menus/:orderby/:sequence', function (req, res) {
+app.get('/mbos/menus/:orderby/:sequence', function(req, res) {
     var orderby = req.params.orderby;
     var sequence = req.params.sequence;
     var valid_orderby = { 'id': 1, 'name': 1, 'price': 1 };
@@ -122,7 +122,7 @@ app.get('/mbos/menus/:orderby/:sequence', function (req, res) {
     });
 });
 
-app.get('/mbos/menu/:id', function (req, res) {
+app.get('/mbos/menu/:id', function(req, res) {
     console.log(`/mbos/menus/${req.params.id}\n`);
 
     db.collection('menus').doc(req.params.id).get().then((doc) => {
@@ -132,7 +132,7 @@ app.get('/mbos/menu/:id', function (req, res) {
     });
 });
 
-app.post('/mbos/menu/create', function (req, res) {
+app.post('/mbos/menu/create', function(req, res) {
     console.log('/mbos/menu/create');
     console.log(req.body);
 
@@ -142,7 +142,7 @@ app.post('/mbos/menu/create', function (req, res) {
     var new_menu = { 'id': req.body.id, 'name': req.body.name, 'price': req.body.price };
     var result = { 'status': null, 'message': null };
 
-    db.collection('menus').doc(new_menu.id.toString()).get().then(function (doc) {
+    db.collection('menus').doc(new_menu.id.toString()).get().then(function(doc) {
         if (doc.exists) {
             result.status = 'fail';
             result.message = `Can't add, menu with ID ${new_menu.id} already exists in the database!`;
@@ -150,7 +150,7 @@ app.post('/mbos/menu/create', function (req, res) {
             res.send(result);
 
         } else {
-            db.collection('menus').doc(new_menu.id.toString()).set(new_menu).then(function (doc) {
+            db.collection('menus').doc(new_menu.id.toString()).set(new_menu).then(function(doc) {
                 result.status = 'success';
                 result.message = `Successful save new menu into database`;
 
@@ -160,7 +160,7 @@ app.post('/mbos/menu/create', function (req, res) {
     });
 });
 
-app.put('/mbos/menu/update/:id', function (req, res) {
+app.put('/mbos/menu/update/:id', function(req, res) {
     console.log(`/mbos/menu/update/${req.params.id}`);
     console.log(req.body);
 
@@ -170,14 +170,14 @@ app.put('/mbos/menu/update/:id', function (req, res) {
     var update_menu = { 'name': req.body.name, 'price': req.body.price };
     var result = { 'status': null, 'message': null };
 
-    db.collection('menus').doc(req.params.id).update(update_menu).then(function (doc) {
+    db.collection('menus').doc(req.params.id).update(update_menu).then(function(doc) {
         result.status = 'success';
         result.message = `Successful update menu into database`;
         res.send(result);
     });
 });
 
-app.delete('/mbos/menu/delete/:id', function (req, res) {
+app.delete('/mbos/menu/delete/:id', function(req, res) {
     console.log(`/mbos/menu/delete/${req.params.id}`);
     console.log(req.body);
 
@@ -187,7 +187,7 @@ app.delete('/mbos/menu/delete/:id', function (req, res) {
     var result = { 'status': null, 'message': null };
 
     // need extra caution here since 'id_menu' field is numeric type (integer)
-    db.collection('itemorders').where('id_menu', '==', parseInt(req.params.id)).get().then(function (docs) {
+    db.collection('itemorders').where('id_menu', '==', parseInt(req.params.id)).get().then(function(docs) {
         console.log(docs.empty);
         if (!docs.empty) {
             result.status = 'fail';
@@ -196,7 +196,7 @@ app.delete('/mbos/menu/delete/:id', function (req, res) {
             res.send(result);
 
         } else {
-            db.collection('menus').doc(req.params.id).delete().then(function (doc) {
+            db.collection('menus').doc(req.params.id).delete().then(function(doc) {
 
                 result.status = 'success';
                 result.message = `Successful delete menu from database`;
@@ -209,14 +209,14 @@ app.delete('/mbos/menu/delete/:id', function (req, res) {
 });
 
 // Paths for customers ////////////////////////////////////
-app.get('/mbos/customers', function (req, res) {
+app.get('/mbos/customers', function(req, res) {
     console.log(`/mbos/customers`);
     res.setHeader('Content-type', 'text/plain');
     res.setHeader('Access-Control-Allow-Origin', '*');
     return res.redirect('/mbos/customers/name/asc');
 });
 
-app.get('/mbos/customers/:orderby/:sequence', function (req, res) {
+app.get('/mbos/customers/:orderby/:sequence', function(req, res) {
     var orderby = req.params.orderby;
     var sequence = req.params.sequence;
     var valid_orderby = { 'name': 1, 'area': 1, 'address': 1 };
@@ -243,7 +243,7 @@ app.get('/mbos/customers/:orderby/:sequence', function (req, res) {
     });
 });
 
-app.get('/mbos/customer/:id', function (req, res) {
+app.get('/mbos/customer/:id', function(req, res) {
     console.log(`/mbos/customers/${req.params.id}\n`);
 
     db.collection('customers').doc(req.params.id).get().then((doc) => {
@@ -254,7 +254,7 @@ app.get('/mbos/customer/:id', function (req, res) {
 });
 
 //Post Customer
-app.post('/mbos/customer/create', async function (req, res) {
+app.post('/mbos/customer/create', async function(req, res) {
     console.log('/mbos/customer/create');
     console.log(req.body);
 
@@ -301,7 +301,7 @@ app.post('/mbos/customer/create', async function (req, res) {
 });
 //End Post Customer
 
-app.delete('/mbos/customer/delete/:id', function (req, res) {
+app.delete('/mbos/customer/delete/:id', function(req, res) {
     console.log(`/mbos/customer/delete/${req.params.id}`);
     console.log(req.body);
 
@@ -311,7 +311,7 @@ app.delete('/mbos/customer/delete/:id', function (req, res) {
     var result = { 'status': null, 'message': null };
 
     // need extra caution here since 'id_menu' field is numeric type (integer)
-    db.collection('customers').where('id_customer', '==', parseInt(req.params.id)).get().then(function (docs) {
+    db.collection('customers').where('id_customer', '==', parseInt(req.params.id)).get().then(function(docs) {
         console.log(docs.empty);
         if (!docs.empty) {
             result.status = 'fail';
@@ -320,7 +320,7 @@ app.delete('/mbos/customer/delete/:id', function (req, res) {
             res.send(result);
 
         } else {
-            db.collection('customers').doc(req.params.id).delete().then(function (doc) {
+            db.collection('customers').doc(req.params.id).delete().then(function(doc) {
 
                 result.status = 'success';
                 result.message = `Successful delete customers from database`;
@@ -330,7 +330,7 @@ app.delete('/mbos/customer/delete/:id', function (req, res) {
     });
 });
 
-app.put('/mbos/customer/update/:id', function (req, res) {
+app.put('/mbos/customer/update/:id', function(req, res) {
     console.log(`/mbos/customer/update/${req.params.id}`);
     console.log(req.body);
 
@@ -340,7 +340,7 @@ app.put('/mbos/customer/update/:id', function (req, res) {
     var update_customer = { 'name': req.body.name, 'address': req.body.address, 'area': req.body.area };
     var result = { 'status': null, 'message': null };
 
-    db.collection('customers').doc(req.params.id).update(update_customer).then(function (doc) {
+    db.collection('customers').doc(req.params.id).update(update_customer).then(function(doc) {
         result.status = 'success';
         result.message = `Successful update customer into database`;
         res.send(result);
@@ -348,14 +348,14 @@ app.put('/mbos/customer/update/:id', function (req, res) {
 });
 
 // Paths for customer orders ////////////////////////////////////
-app.get('/mbos/custorders', function (req, res) {
+app.get('/mbos/custorders', function(req, res) {
     res.setHeader('Content-type', 'text/plain');
     res.setHeader('Access-Control-Allow-Origin', '*');
     console.log(`/mbos/custorders`);
     return res.redirect('/mbos/custorders/date_time/desc');
 });
 
-app.get('/mbos/custorders/:orderby/:sequence', function (req, res) {
+app.get('/mbos/custorders/:orderby/:sequence', function(req, res) {
     var orderby = req.params.orderby;
     var sequence = req.params.sequence;
     var valid_orderby = { 'date_time': 1 };
@@ -380,7 +380,7 @@ app.get('/mbos/custorders/:orderby/:sequence', function (req, res) {
     });
 });
 
-app.get('/mbos/custorders/:id_customer', function (req, res) {
+app.get('/mbos/custorders/:id_customer', function(req, res) {
     var id_customer = req.params.id_customer;
 
     console.log(`/mbos/custorders/${id_customer}\n`);
@@ -402,7 +402,7 @@ app.get('/mbos/custorders/:id_customer', function (req, res) {
 
 function getItems(custorders, res) {
     var order_count = 0;
-    custorders.forEach(function (odr, idx) {
+    custorders.forEach(function(odr, idx) {
         //console.log(odr);
 
         // to store items ordered for each customer's order
@@ -437,14 +437,14 @@ function getItems(custorders, res) {
 }
 
 //Add new custorder
-app.post('/mbos/custorder/create', async function (req, res) {
+app.post('/mbos/custorder/create', async function(req, res) {
     console.log('/mbos/custorder/create');
     console.log(req.body);
 
     res.setHeader('Content-type', 'text/plain');
     res.setHeader('Access-Control-Allow-Origin', '*');
 
-    var custId,custorderId;
+    var custId, custorderId;
     var date = new Date();
     var datetime = dateFormat(date, "yyyy-mm-dd h:MM:ss");
     const snapshot = await db.collection('customers').where('name', '==', req.body.name.toString()).get();
@@ -477,43 +477,39 @@ app.post('/mbos/custorder/create', async function (req, res) {
 
     //     }
     // });
-    
-    if(snapshot.empty)
-    {
+
+    if (snapshot.empty) {
         result.status = 'fail';
         result.message = `Error in completing task!`;
         res.send(result);
-    }
-    else
-    {
+    } else {
         snapshot.forEach(docs => {
-                custId = docs.id;
-            });
+            custId = docs.id;
+        });
 
         const newcustorder = await db.collection('custorders').add({
-                id_customer: custId,
-                date_time: datetime
-            })
+            id_customer: custId,
+            date_time: datetime
+        })
 
 
         custorderId = newcustorder.id;
     }
-    
+
     var ordrlist = JSON.parse(req.body.orderlist);
     console.log(ordrlist.length);
-    for(let i=0; i<ordrlist.length; i++)
-    {   
+    for (let i = 0; i < ordrlist.length; i++) {
         // console.log(custorderId);
         console.log(ordrlist[i].item);
-         db.collection('menus').where('name', '==', ordrlist[i].item.toString()).get().then(
-            function (docs){
+        db.collection('menus').where('name', '==', ordrlist[i].item.toString()).get().then(
+            function(docs) {
                 docs.forEach(item => {
                     console.log(item.id);
                     db.collection('itemorders').add({
                         id_custorder: custorderId,
                         id_menu: item.id,
                         qty: ordrlist[i].qty
-                     });
+                    });
                 });
 
             }
@@ -528,8 +524,69 @@ app.post('/mbos/custorder/create', async function (req, res) {
 });
 //End Add new custorder
 
+//Delete custorder
+app.delete('/mbos/custorder/delete/:id', function(req, res) {
+    console.log(`/mbos/custorder/delete/${req.params.id}`);
+    console.log(req.body);
+
+    res.setHeader('Content-type', 'text/plain');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    var result = { 'status': null, 'message': null };
+
+    db.collection('itemorders').where('id_custorder', '==', req.params.id).get().then(function(docs) {
+        if (!docs.empty) {
+            docs.forEach(item => {
+                db.collection('itemorders').doc(item.id).delete();
+            });
+
+            db.collection('custorders').doc(req.params.id).delete().then(function(docs) {
+                result.status = 'success';
+                result.message = 'Successful delete custorder from database';
+
+                res.send(result);
+            });
+        } else {
+            result.status = 'fail';
+            result.message = 'Server Error. Please Try Again';
+
+            res.send(result);
+        }
+    });
+});
+//End delete custorder
+
+//Order custorders in sequence
+app.get('/mbos/custorders/:orderby/:sequence', function(req, res) {
+    var orderby = req.params.orderby;
+    var sequence = req.params.sequence;
+    var valid_orderby = { 'date_time': 1 };
+
+    if (!valid_orderby[orderby]) orderby = 'date_time';
+    if (sequence != 'desc') sequence = 'asc';
+
+    console.log(`/mbos/custorders/${orderby}/${sequence}\n`);
+
+    db.collection('custorders').orderBy(orderby, sequence).get().then((docs) => {
+        var custorders = [];
+        docs.forEach(doc => {
+            //console.log(doc.data().name);
+            var data = doc.data();
+            data.id = doc.id; // add doc id as part of doc data
+            custorders.push(data);
+        });
+
+        var pretty_json = JSON.stringify(custorders, null, 2);
+
+        res.setHeader('Content-type', 'text/plain');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.send(pretty_json);
+    });
+});
+//End order custorders in sequence
+
 // Paths for menu items ordered ////////////////////////////////////
-app.get('/mbos/itemorders', function (req, res) {
+app.get('/mbos/itemorders', function(req, res) {
     console.log(`/mbos/itemorders`);
 
     db.collection('itemorders').get().then((docs) => {
@@ -549,7 +606,7 @@ app.get('/mbos/itemorders', function (req, res) {
     });
 });
 
-app.get('/mbos/itemorders/:id_custorder', function (req, res) {
+app.get('/mbos/itemorders/:id_custorder', function(req, res) {
     var id_custorder = req.params.id_custorder;
 
     console.log(`/mbos/itemorders/${id_custorder}`);
@@ -572,7 +629,7 @@ app.get('/mbos/itemorders/:id_custorder', function (req, res) {
 });
 
 // Start the server ///////////////////////////////////////////////////////////
-var server = app.listen(8080, function () {
+var server = app.listen(8080, function() {
     var port = this.address().port;
     console.log("Express HTTP server listening at http://localhost:%s", port);
 });
